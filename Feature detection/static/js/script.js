e0570e0c974955ef52e63e88095dd515c532a387
Unknown <slqhoeks@gmail.com>
2018-03-21 16:50:29 +0100

    update

--- Feature detection/static/js/script.js
+++ Feature detection/static/js/script.js
@@ -10,9 +10,9 @@
     }
     var detect =  {
         device: function() {
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            /*if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
                 $('body').addClass('isMobile')
-            }
+            }*/
         }
     }
     var sparkle = {
