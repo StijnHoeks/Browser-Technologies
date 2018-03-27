f5e26630045df559414062251d62919dbf122318
Unknown <slqhoeks@gmail.com>
2018-03-27 17:35:46 +0200

    update

---
+++ Progressive enhancement/server.js
@@ -0,0 +1,51 @@
+var express = require('express')
+var request = require('request')
+var app = express()
+var host = 'https://randomuser.me/api/?results=50&nat=nl';
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', function (req, res) {
+  request(host, function (error, response, body) {
+    var data = JSON.parse(body);
+    var results = data.results;
+
+    results.sort(function(a, b) {
+		var nameA = a.name.first.toUpperCase(); // ignore upper and lowercase
+		var nameB = b.name.first.toUpperCase(); // ignore upper and lowercase
+		if (nameA < nameB) {
+			return -1;
+		}
+		if (nameA > nameB) {
+			return 1;
+		}
+		// names must be equal
+		return 0;
+	});
+    
+    var lastFirstLetter;
+    var templateData = [];
+   	var dataPerLetter = null;
+    results.forEach(function (result) {
+    	if (lastFirstLetter !== result.name.first.charAt(0)) {
+    		if (dataPerLetter !== null) {
+    			templateData.push(dataPerLetter);
+    		}
+    		dataPerLetter = {
+    			letter: result.name.first.charAt(0),
+    			data: []
+    		}
+    	}
+    	dataPerLetter.data.push(result.name);
+    	lastFirstLetter = result.name.first.charAt(0);
+    });    
+
+    res.render('index', {data: templateData})
+  });
+})
+
+var server = app.listen(1337, function () {
+   console.log('server is running on port 1337')
+})
--- Progressive enhancement/static/js/script.js
+++ Progressive enhancement/static/js/script.js
@@ -6,79 +6,9 @@
     var app = {
         init: function() {
 
-
         }
     }
-    /*var api = {
-        init: function() {
-            this.request();
-        },
-        request: function() {
-            // api_key=dbuOrGB7xoks2WobqPacpFP6fODFIU7gR0rStswa
-            var self = this;
-            var request = new XMLHttpRequest();
-            request.open('GET', 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=dbuOrGB7xoks2WobqPacpFP6fODFIU7gR0rStswa', true);
 
-            request.onload = function() {
-                if (request.status >= 200 && request.status < 400) {
-                    var rawData = JSON.parse(request.responseText).photos;
-                    collection.clean(rawData);
-                } else {
-                    api.error();
-                }
-            };
-            request.onerror = function() {
-                api.error();
-            };
-            request.send(); 
-        },
-        error: function(rawData) {
-
-        }
-    }
-    var collection = {
-        clean: function(rawData) {
-
-            var data = rawData.filter( function(currentValue, index, arr){
-                if ( index/2 % 1 === 0 ) { 
-                    return currentValue
-                }
-            });
-            template.main(data);
-
-        }
-    }
-    var template = {
-        main: function(data) {
-            for (var i = 0; i < data.length; i++) {
-                var sec = document.querySelector('section');
-                var a = document.createElement("a");
-                var h = document.createElement("h1");
-                var p = document.createElement("p");
-                var img = document.createElement("img");
-                a.setAttribute('href','index.html')
-                img.setAttribute('src',data[i].img_src);
-                h.innerHTML = data[i].camera.full_name;
-                p.innerHTML = data[i].img_src;
-                a.appendChild(img);
-                a.appendChild(h);
-                a.appendChild(p);
-                sec.appendChild(a); 
-            }
-        },
-        detail: function() {
-
-        }
-    }
-    var render = {
-        main: function() {
-
-        },
-        detail: function() {
-
-        }
-    }*/
-
-    //app.init()
+    app.init()
 
 })()
